51536a8c341a0b1041b70e16f7990dcba7798363
Jani Pekkala <jani.pekkala@student.careeria.fi>
2024-02-02 18:05:13 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+//Express js palvelin
+//otetaan kirjasto käyttöön
+const path = require('path')
+const express = require ('express')
+const app = express()
+const herkut = require('./herkut.json')
+
+app.get('/api/herkut', (req, res) => {
+    res.json(herkut)
+    })
+//path.join metodi(parametrit) = (mihin liitetään , mitä liitetään)
+const polku = path.join(__dirname, '/public') //public nimisestä kansiosta löytyy selaimelle lähetettävät tiedostot 
+//sanotaan että em. polussa on tiedostosisältö jota palvelin käyttää kun se saa http requestin
+//get pyyntö
+app.use(express.static(polku)) //parametrina polku muuttujan arvot
+//.listen metodi ottaa vastaan 2 kaksiparametria 3000 ja funktio (lambda)
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.') //ilmoitus konsoliin mihin porttiin serveri on asetettu
+})
