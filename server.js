e2a6628cbcb081b7a77a8443eea418aa82ba03c1
Jani Pekkala <jani.pekkala@student.careeria.fi>
2024-02-04 16:52:14 +0200

    Tehty css muotoiluja

--- server.js
+++ server.js
@@ -2,12 +2,30 @@
 //otetaan kirjasto käyttöön
 const path = require('path')
 const express = require ('express')
+const fs = require('fs').promises
 const app = express()
 const herkut = require('./herkut.json')
 
 app.get('/api/herkut', (req, res) => {
     res.json(herkut)
     })
+
+//-------------------------------
+//----- Pin koodin luku
+app.get('/api/getpin',async (req,res) => {
+    try {
+
+        const savedPin = await fs.readFile('./pin.txt','utf-8')
+        res.send(savedPin)
+
+    } catch (error) {
+        console.error('Error reading file:', error)
+        res.status(500).send('Internal Server Error')
+    }
+})
+
+
+
 //path.join metodi(parametrit) = (mihin liitetään , mitä liitetään)
 const polku = path.join(__dirname, '/public') //public nimisestä kansiosta löytyy selaimelle lähetettävät tiedostot 
 //sanotaan että em. polussa on tiedostosisältö jota palvelin käyttää kun se saa http requestin
